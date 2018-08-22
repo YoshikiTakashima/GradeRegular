52f7d70ade331aee04d09a8f7e6f27ac227e9897
Yoshiki Takashima <takashima.yoshiki@hotmail.com>
2018-08-22 11:33:53 -0700

    added distance and blur utils

--- MyLibrary/ImageUtils.py
+++ MyLibrary/ImageUtils.py
@@ -1,34 +1,41 @@
-import cv2 as cv
+import cv2
 import numpy as np
 from matplotlib import pyplot as plt
+import math
 
 def toGreyImg(img):
-    return cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+    return cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
 def binarize(img):
-    grayImage = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
-    ret,thresh = cv.threshold(grayImage, 127, 255, cv.THRESH_BINARY)
+    grayImage = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    ret,thresh = cv2.threshold(grayImage, 127, 255, cv2.THRESH_BINARY)
     return thresh
 
-def denoise(img):
-    return(cv.GaussianBlur(img,(5,5),0))
+def denoiseGaussian(img):
+    return(cv2.GaussianBlur(img,(5,5),0))
+
+def denoiseMedian(img):
+    return cv2.medianBlur(img,5)
+
+def distanceBetween(xy1, xy2):
+    return math.hypot((xy1[0] - xy2[0]), (xy1[1] - xy2[1]))
 
 def show(img):
-    cv.imshow('Lines',img)
-    cv.waitKey(0)
-    cv.destroyAllWindows()
+    cv2.imshow('Lines',img)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
 
 def main():
     #from sys import argv
-    img = cv.imread('./Examples/UtilTest/handDFA.jpg')
+    img = cv2.imread('./Examples/UtilTest/handDFA.jpg')
 
     img = toGreyImg(img)
     #img = denoise(img)
     img = binarize(img)
 
-    cv.imshow('Binarized',img)
-    cv.waitKey(0)
-    cv.destroyAllWindows()
+    cv2.imshow('Binarized',img)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
 
 if __name__ == '__main__':
     main()
