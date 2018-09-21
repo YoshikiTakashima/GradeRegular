ce61df457dd6d31ffa787124314dc1a775c3c86d
Yoshiki Takashima <takashima.yoshiki@hotmail.com>
2018-09-20 17:54:06 -0700

    Added JFLAP to Verilog function.

--- JFLAP2V.py
+++ JFLAP2V.py
@@ -1,13 +1,51 @@
-class State:
-    def __init__(self):
-        self.zero = None
-        self.one = None
+import xml.etree.ElementTree
+from Image2V import transitionSetToStateValueMap
+import MyLibrary.VerilogUtil as VUtil
+
+def xmlRoot2Verilog(root):
+    auto = root.findall("automaton")[0]
+    # print(auto)
+
+    states = []
+    for s in auto.findall("state"):
+        lbl = 'NA'
+        x = int(round(float(s[0].text)))
+        y = int(round(float(s[1].text)))
+        init = s.findall("initial")
+        final = s.findall("final")
+        if len(final) > 0:
+            lbl = 'A'
+        
+        if len(init) > 0:
+            states = [(x, y, 1, lbl)] + states
+        else:
+            states.append((x, y, 1, lbl))
     
-    def isComplete(self):
-        return (self.zero != None) and (self.one != None)
+    transitions = []
+    for t in auto.findall("transition"):
+        frm = int(t[0].text)
+        to = int(t[1].text)
+        val = t[2].text
+        # print("frm: {}, to: {}, val: {}".format(frm, to, val))
 
-    def setZero(self, zeroState):
-        self.zero = zeroState
+        val = t[2].text
+        if val == '0':
+            val = 0
+        elif val == '1':
+            val = 1
+        else:
+            val = 2
+        transitions.append(((frm, -1), (to, val)))
 
-    def setOne(self, oneState):
-        self.one = oneState
+    encoded = transitionSetToStateValueMap(states, transitions)
+    return VUtil.transitionToVerilog(states, encoded)
+
+def main():
+    from sys import argv
+    e = xml.etree.ElementTree.parse(argv[1]).getroot()
+
+    print(xmlRoot2Verilog(e))
+
+
+if __name__ == '__main__':
+    main()
